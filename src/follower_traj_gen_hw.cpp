2b35da5ae228b41c106d5609cd95abdbe7a8e2de
Suchitha Channapatna <channapatna.suchitha@aurora.aero>
2026-08-05 16:12:15 -0400

    Update hw files for velocity tracking & CBF

--- src/follower_traj_gen_hw.cpp
+++ src/follower_traj_gen_hw.cpp
@@ -41,9 +41,13 @@ public:
         this->declare_parameter<double>("max_speed", 0.5);
         this->declare_parameter<double>("deadzone_vctrl", 0.05);
         this->declare_parameter<std::string>("follower_topic_pos", "world");
-
         // Initial position of follower drone with respect to map frame
         this->declare_parameter<std::vector<double>>("init_follower_offset", {0.0, 3.0, 0.0}); 
+        // For CBF
+        this->declare_parameter<double>("cbf_dmin", 0.0);
+        this->declare_parameter<double>("cbf_dmax", 0.0);
+        this->declare_parameter<double>("cbf_alpha", 0.0);
+        this->declare_parameter<bool>("use_cbf", false);
 
         this->get_parameter("follower_mode", follower_mode_);
         this->get_parameter("follower_distance_T", follower_distance_T_);
@@ -57,6 +61,11 @@ public:
         this->get_parameter("max_speed", u_follower_max_);
         this->get_parameter("deadzone_vctrl", deadband_);
         this->get_parameter("follower_topic_pos", follower_topic_pos_);
+        // For CBF
+        this->get_parameter("cbf_dmin", d_min_);
+        this->get_parameter("cbf_dmax", d_max_);
+        this->get_parameter("cbf_alpha", alpha_);
+        this->get_parameter("use_cbf", use_cbf_);
 
         double publish_freq;
         this->get_parameter("publish_freq", publish_freq);
@@ -165,12 +174,12 @@ private:
 
         Eigen::Quaterniond L_q = quatFromMsg(L_qmsg);
 
-        // Leader/follower positions (note here map and world are the same - TODO: go back and use consistent wording)
+        // Leader/follower positions
         Eigen::Vector3d leader_pos_world(L_pos.x, L_pos.y, L_pos.z);
         Eigen::Vector3d follower_pos_world(F_pos.x, F_pos.y, F_pos.z);
         Eigen::Vector3d leader_vel_world(L_vel.x, L_vel.y, L_vel.z);
 
-        // Offset in leader body frame: behind leader in T, and optionally lateral/vertical offsets
+        // Offset in leader body frame
         Eigen::Vector3d desired_offset_tnb(
             follower_distance_T_,
             follower_distance_N_,
@@ -180,13 +189,6 @@ private:
         // desired position of the follower in the world frame
         Eigen::Vector3d desired_p_follower = leader_pos_world + L_q * desired_offset_tnb;
 
-        RCLCPP_INFO_THROTTLE(
-            this->get_logger(),
-            *this->get_clock(),
-            1000,
-            "Desired offset stnb (%.2f, %.2f, %.2f) , LQ (%.2f, %.2f, %.2f)",
-            desired_offset_tnb.x(), desired_offset_tnb.y(), desired_offset_tnb.z(),
-            L_q.w(), L_q.x(), L_q.y(), L_q.z());
 
         // Relative displacement from follower to leader in world
         Eigen::Vector3d rel_world = follower_pos_world - leader_pos_world;
@@ -206,38 +208,69 @@ private:
         double uT = leader_vel_tnb.x() + u_follower_max_ * sT / std::sqrt(sT*sT + p_tuning_ * p_tuning_);
         double uN = leader_vel_tnb.y() + u_follower_max_ * sN / std::sqrt(sN*sN + p_tuning_ * p_tuning_);
         double uB = leader_vel_tnb.z() + u_follower_max_ * sB / std::sqrt(sB*sB + p_tuning_ * p_tuning_);
+        
+        if (use_cbf_){
 
+            // Set nominal velocities to 0. 
+            // Because in this demo, we are showing how CBFs can be used for formation flight without a nominal input
+            uT = 0.0;
+            uN = 0.0;
+            uB = 0.0;
+            Eigen::Vector3d follower_u_tnb(uT, uN, uB); // the nominal inputs
+            // Let the moving barrier constraint be projected onto the desired follower positions
+            Eigen::Vector3d vel_err_tnb = follower_u_tnb - leader_vel_tnb;
+            Eigen::Vector3d relative_sep = - e_form; // relative separation between the desired and actual follower position (x_F - (x_L + desired_offset))
+            // Compute lagrangian multipliers
+            double delH1_x = vel_err_tnb.x() + alpha_*(relative_sep.x() - d_max_);
+            double delH2_x = -vel_err_tnb.x() + alpha_*(-relative_sep.x() - d_max_);
+            double delH1_y = vel_err_tnb.y() + alpha_*(relative_sep.y() - d_max_);
+            double delH2_y = -vel_err_tnb.y() + alpha_*(-relative_sep.y() - d_max_);
+            double delH1_z = vel_err_tnb.z() + alpha_*(relative_sep.z() - d_max_);
+            double delH2_z = -vel_err_tnb.z() + alpha_*(-relative_sep.z() - d_max_);
+            double lambda1_x = 2*std::max(0.0, delH1_x);
+            double lambda2_x = 2*std::max(0.0, delH2_x);
+            double lambda1_y = 2*std::max(0.0, delH1_y);
+            double lambda2_y = 2*std::max(0.0, delH2_y);
+            double lambda1_z = 2*std::max(0.0, delH1_z);
+            double lambda2_z = 2*std::max(0.0, delH2_z);
+            // Compute the augmentation.
+            double pi_x = 0.5 * (lambda2_x - lambda1_x);
+            double pi_y = 0.5 * (lambda2_y - lambda1_y);
+            double pi_z = 0.5 * (lambda2_z - lambda1_z);
+
+            uT += pi_x;
+            uN += pi_y;
+            uB += pi_z;
+            RCLCPP_INFO(
+            this->get_logger(),
+            "current pos error (%.2f, %.2f %.2f), velocities (%.2f, %.2f, %.2f)",
+            std::abs(e_form.x()), std::abs(e_form.y()), std::abs(e_form.z()), uT, uN, uB);
+        }
+        // Saturate velocity commands that exceed max_speed
         Eigen::Vector3d v_TNB(uT,uN,uB);
         double norm = v_TNB.norm();
         norm = std::fabs(norm);
         if (norm > u_follower_max_ && norm  > 1e-9) {
-            if(norm > 1) {
-                v_TNB *= (u_follower_max_ / norm);
-            }
-            else {
-                 v_TNB *= (u_follower_max_ * norm);
-            }
+            RCLCPP_INFO(this->get_logger(), "SATURATED");
+            v_TNB *= (u_follower_max_ / norm);
         }
+
         // Get velocity back into world frame
-        Eigen::Vector3d v_world = L_q * v_TNB;
+        Eigen::Vector3d v_world_desired = L_q * v_TNB;
 
         snapstack_msgs2::msg::Goal goal;
         goal.header.stamp = this->now();
 
         // position target
-        goal.p.x = desired_p_follower.x(); 
-        goal.p.y = desired_p_follower.y(); 
-        goal.p.z = desired_p_follower.z(); 
+        goal.p.x = NAN; //desired_p_follower.x(); 
+        goal.p.y = NAN; //desired_p_follower.y(); 
+        goal.p.z = NAN; //desired_p_follower.z(); 
 
         // accel / jerk can be zero for now
 
-        // goal.v.x = 0.0;//
-        // goal.v.y = 0.0;//
-        // goal.v.z = 0.0;//
-
-        goal.v.x = v_world.x();
-        goal.v.y = v_world.y();
-        goal.v.z = v_world.z();
+        goal.v.x = v_world_desired.x();
+        goal.v.y = v_world_desired.y();
+        goal.v.z = v_world_desired.z();
 
         goal.a.x = 0.0;
         goal.a.y = 0.0;
@@ -252,18 +285,10 @@ private:
         goal.dpsi = 0.0;
 
         goal.power = true;
-        goal.mode_xy = snapstack_msgs2::msg::Goal::MODE_POSITION_CONTROL;
-        goal.mode_z = snapstack_msgs2::msg::Goal::MODE_POSITION_CONTROL;
+        goal.mode_xy = snapstack_msgs2::msg::Goal::MODE_VELOCITY_CONTROL;
+        goal.mode_z = snapstack_msgs2::msg::Goal::MODE_VELOCITY_CONTROL;
 
         goal_pub_->publish(goal);
-
-        RCLCPP_INFO_THROTTLE(
-            this->get_logger(),
-            *this->get_clock(),
-            1000,
-            "Leader (%.2f, %.2f, %.2f) -> Goal (%.2f, %.2f, %.2f)",
-            L_pos.x, L_pos.y, L_pos.z,
-            goal.p.x, goal.p.y, goal.p.z);
     }
 
     double quat2yaw(const geometry_msgs::msg::Quaternion& q)
@@ -310,6 +335,12 @@ private:
     std::vector<double> init_follower_offset_;
     double p_tuning_;
     double deadband_;
+
+    // FOR CBF
+    double d_min_;
+    double d_max_;
+    double alpha_;
+    bool use_cbf_;
 
 };
 
